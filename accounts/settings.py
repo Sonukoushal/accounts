6e90ec2f2e625ec4de4031a92fb943e048522028
Sonu Koushal <sonukoushal3046@gmail.com>
2025-06-30 23:52:08 +0530

    Updated settings.py for CORS config

--- accounts/settings.py
+++ accounts/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-7ym^^=7xoabtdzaf&*5%vocq0-=(nx#k#-u5_s6f*l!e5d&3ev
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 
 # Application definition
